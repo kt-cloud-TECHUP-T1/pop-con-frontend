abcae28cb473674ba53d3c6c2ca24786bc27f08b
김민중 <f1minn2r@naver.com>
2026-02-13 14:47:15 +0900

    feat: CommonCheckbox 컴포넌트 및 관련 아이콘 추가

---
+++ src/components/Icon/icons/Checkbox.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgCheckbox = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M3 7a4 4 0 0 1 4-4h10a4 4 0 0 1 4 4v10a4 4 0 0 1-4 4H7a4 4 0 0 1-4-4z"
+    />
+    <path
+      fill="currentColor"
+      fillOpacity={0.16}
+      d="M17 3a4 4 0 0 1 4 4v10a4 4 0 0 1-3.794 3.995L17 21H7l-.206-.005a4 4 0 0 1-3.79-3.789L3 17V7a4 4 0 0 1 4-4zM7 4.5A2.5 2.5 0 0 0 4.5 7v10A2.5 2.5 0 0 0 7 19.5h10a2.5 2.5 0 0 0 2.5-2.5V7A2.5 2.5 0 0 0 17 4.5z"
+    />
+  </svg>
+);
+export default SvgCheckbox;
