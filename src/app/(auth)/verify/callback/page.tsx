5719d9cc98d0b5751a87dd0341c1b6d5d4320ecd
김민중 <f1minn2r@naver.com>
2026-02-24 15:41:11 +0900

    feat: auth/protected 라우트 그룹 및 레이아웃 구조 추가

---
+++ src/app/(auth)/verify/callback/page.tsx
@@ -0,0 +1,5 @@
+// 포트원 콜백 처리
+
+export default function verifyCallback() {
+  return <div>본인 인증 콜백 페이지</div>;
+}
---
+++ src/app/(protected)/page.tsx
@@ -0,0 +1,5 @@
+// protected -> 로그인 필수 영역
+
+export default function test() {
+  return <div>test</div>;
+}
