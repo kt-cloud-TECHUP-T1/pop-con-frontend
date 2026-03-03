65e78ae0b54047177d7be74c0bd7484d1781d3aa
Won Chan LEE <gxpk2978@naver.com>
2026-03-03 10:06:38 +0900

    refactor : 소셜 로그인 image태그 수정 및 위치조정

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,15 +3,12 @@
 import { Wrapper } from '@/components/layout/wrapper';
 import { Button } from '@/components/ui/button';
 import { Typography } from '@/components/ui/typography';
+import Image from 'next/image';
 
 export default function Login() {
   return (
-    <Wrapper>
-      <Typography
-        weight="bold"
-        variant="heading-1"
-        className="text-center pt-3xl"
-      >
+    <Wrapper className="pt-[120px]">
+      <Typography weight="bold" variant="heading-1" className="text-center">
         로그인
       </Typography>
 
@@ -19,7 +16,14 @@ export default function Login() {
         <Button
           size="large"
           className="w-full bg-[#FEE500] hover:bg-[#FEE500] active:bg-[#FEE500]"
-          leftIcon={<img src="/icons/kakao.svg" className="w-6 h-6"></img>}
+          leftIcon={
+            <Image
+              src="/icons/kakao.svg"
+              width={24}
+              height={24}
+              alt="카카오"
+            ></Image>
+          }
         >
           <Typography variant="label-1" weight="medium" className="text-black">
             카카오 로그인
@@ -28,7 +32,14 @@ export default function Login() {
         <Button
           size="large"
           className="w-full bg-[#03C75A] hover:bg-[#03C75A] active:bg-[#03C75A]"
-          leftIcon={<img src="/icons/naver.svg" className="w-6 h-6"></img>}
+          leftIcon={
+            <Image
+              src="/icons/naver.svg"
+              width={24}
+              height={24}
+              alt="네이버"
+            ></Image>
+          }
         >
           <Typography variant="label-1" weight="medium">
             네이버 로그인
