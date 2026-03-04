1d2b68590482f2a275bc7bdd45de91a2a0ebc57a
gentlee <64201460+leegentle@users.noreply.github.com>
2026-03-04 13:41:29 +0900

    fix : middleware.ts 부분주석으로 변경

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,13 @@
-// import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-// export function middleware(request: NextRequest) {
-//     const registerToken = request.cookies.get('registerToken')?.value;
-//     return registerToken ?
-//         NextResponse.next() :
-//         NextResponse.redirect(new URL('/login', request.url));
-//     }
-
-// export const config = {
-//   matcher: ['/signup', '/verify'],
-// };
+export function middleware(request: NextRequest) {
+    return NextResponse.next();
+    // const registerToken = request.cookies.get('registerToken')?.value;
+    // return registerToken ?
+    //     NextResponse.next() :
+    //     NextResponse.redirect(new URL('/login', request.url));
+    }
+    
+export const config = {
+  matcher: ['/signup', '/verify'],
+};
