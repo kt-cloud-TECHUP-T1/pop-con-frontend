7e0d01a813c02b2799d65812496910f8ec86950f
김민중 <f1minn2r@naver.com>
2026-03-19 12:35:12 +0900

    feat: 보안 퀴즈 화면과 전역 provider 연결 추가

---
+++ src/app/security-quiz/page.tsx
@@ -0,0 +1,5 @@
+import { SecurityQuizPageClient } from '@/features/security-quiz/components/security-quiz-page-client';
+
+export default function SecurityQuizPage() {
+  return <SecurityQuizPageClient />;
+}
