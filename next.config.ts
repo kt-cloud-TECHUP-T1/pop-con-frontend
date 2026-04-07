b6fc2db64ce97505cb4e7f7b40db29b00667b832
Won Chan LEE <gxpk2978@naver.com>
2026-04-07 12:04:05 +0900

    refactor : 불필요한 api 호출 및 이미지 도메인 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   output: 'standalone',
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        //추후 이미지 저장된 서버 도메인으로 변경
+        hostname: 'imagelink.com',
+      },
+    ],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
