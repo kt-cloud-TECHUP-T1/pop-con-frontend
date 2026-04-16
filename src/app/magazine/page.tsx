6350b7e4fae73316de1a8607a2f8dbb808e43cd3
Won Chan LEE <gxpk2978@naver.com>
2026-04-16 11:44:49 +0900

    feat : 매거진 임시 수정

--- src/app/magazine/page.tsx
+++ src/app/magazine/page.tsx
@@ -1,5 +1,11 @@
 import { Wrapper } from '@/components/layout/wrapper';
+import { Button } from '@/components/ui/button';
 
 export default function MagazinePage() {
-  return <Wrapper>매거진 페이지입니다.</Wrapper>;
+  return (
+    <Wrapper>
+      매거진 페이지입니다.
+      <Button>임시버튼 </Button>
+    </Wrapper>
+  );
 }
